fd6c584393ad4d7fa63ea2ee0851488edfe474ad
ovid-nis <jacobsonnenberg0@gmail.com>
2014-11-21 15:54:35 -0600

    wrote some more on staff

--- Project361/src/edu/uwm/cs361/Staff.java
+++ Project361/src/edu/uwm/cs361/Staff.java
@@ -2,8 +2,14 @@ package edu.uwm.cs361;
 
 import java.util.ArrayList;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @PersistenceCapable
 public class Staff {
@@ -20,6 +26,60 @@ public class Staff {
 	private String permissions;
 	@Persistent
 	private String password;
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	public Staff(String em, String na, ArrayList<Section> secs, String of, String perm, String pass){
+		email = em;
+		name = na;
+		sectionsTaught = secs;
+		officeHours = of;
+		permissions = perm;
+		password = pass;
+		
+		key = KeyFactory.createKey(Staff.class.getSimpleName(), name);
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		pm.makePersistent(this);
+		pm.close();
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Section> getSectionsTaught() {
+		return sectionsTaught;
+	}
+	public void setSectionsTaught(ArrayList<Section> sectionsTaught) {
+		this.sectionsTaught = sectionsTaught;
+	}
+	public String getOfficeHours() {
+		return officeHours;
+	}
+	public void setOfficeHours(String officeHours) {
+		this.officeHours = officeHours;
+	}
+	public String getPermissions() {
+		return permissions;
+	}
+	public void setPermissions(String permissions) {
+		this.permissions = permissions;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
 }
